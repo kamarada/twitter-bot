2cd1a006d5ec1071c662103eaf6d034affefca12
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2020-12-11 00:41:10 -0300

    Olá, Twitter!

--- robo.js
+++ robo.js
@@ -1,1 +1,27 @@
-console.log ('Olá, mundo!');
+const Twit = require('twit');
+
+require('dotenv').config();
+
+function tuitar(mensagem) {
+    var twitter = new Twit({
+        consumer_key:         process.env.CONSUMER_KEY,
+        consumer_secret:      process.env.CONSUMER_SECRET,
+        access_token:         process.env.ACCESS_TOKEN,
+        access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+        timeout_ms:           60*1000,
+        strictSSL:            true
+    });
+    twitter.post('statuses/update',
+    {
+        status: mensagem
+    },
+    function(err, data, response) {
+        console.log(data)
+    });
+}
+
+async function main() {
+    tuitar('Olá, Twitter!');
+}
+
+main();
