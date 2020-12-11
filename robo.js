4e96df9bbfb3f11c0c980802959ca5060a332411
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2020-12-11 00:58:33 -0300

    Obtendo números do site do governo com puppeteer

--- robo.js
+++ robo.js
@@ -1,8 +1,29 @@
 const Cron = require('node-cron');
 
+const puppeteer = require('puppeteer');
+
 const Twit = require('twit');
 
 require('dotenv').config();
+
+async function obterNumeroDeRecuperados() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1024,
+        height: 768,
+    });
+    await page.goto('https://covid.saude.gov.br/', {
+        timeout: 60*1000,
+        waitUntil: 'networkidle2'
+    });
+    var recuperados = await page.evaluate(() => {
+        var numeroNaPagina = document.getElementsByClassName('lb-total')[0].lastChild.data.trim();
+        return numeroNaPagina
+    });
+    await browser.close();
+    return recuperados;
+}
 
 function tuitar(mensagem) {
     var twitter = new Twit({
@@ -23,11 +44,8 @@ function tuitar(mensagem) {
 }
 
 async function main() {
-    tuitar('Olá, Twitter!');
+    var recuperados = await obterNumeroDeRecuperados();
+    console.log(recuperados);
 }
 
-// main();
-
-Cron.schedule('*/2 * * * * *', () => {
-    console.log('Esse agendamento roda segundo sim, segundo não');
-});
+main();
